187e20db4e69a6de9eedc983eed284ac48cd04af
Lukasz Wiklendt <lwiklendt@gmail.com>
2019-08-26 15:32:11 +0930

    Added Heap's algorithm for generating permutations as a generator for swapping elements

--- lsw/collect.py
+++ lsw/collect.py
@@ -8,3 +8,24 @@ def flatten(l):
             yield from flatten(el)
         else:
             yield el
+
+
+def permutation_swaps(n):
+    """
+    Heap's algorithm for generating permutation swaps.
+    :param n: number of elements to permute
+    :return: generator yielding a pair of indexes for which pair of elements should be swapped
+    """
+    c = [0] * n
+    i = 0
+    while i < n:
+        if c[i] < i:
+            if i % 2 == 0:
+                yield (0, i)
+            else:
+                yield (c[i], i)
+            c[i] += 1
+            i = 0
+        else:
+            c[i] = 0
+            i += 1
