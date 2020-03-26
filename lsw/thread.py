ee994886442e49c844d70587a8fb6e097fe6128b
Lukasz Wiklendt <lwiklendt@gmail.com>
2020-03-26 13:16:36 +1030

    switch parexec default num threads to cpu_count() rather than num blocks

--- lsw/thread.py
+++ lsw/thread.py
@@ -1,8 +1,9 @@
 from concurrent import futures
+import multiprocessing
 
 
 def parexec(exec_func, nblocks, nthreads=None):
     if nthreads is None:
-        nthreads = nblocks
+        nthreads = multiprocessing.cpu_count()
     with futures.ThreadPoolExecutor(nthreads) as executor:
         futures.wait([executor.submit(exec_func, i) for i in range(nblocks)])
