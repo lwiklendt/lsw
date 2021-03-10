a9eeabf7df35550e61bf241c2847fb038658f9bc
Lukasz Wiklendt <lwiklendt@gmail.com>
2021-03-10 13:51:26 +1030

    add pickle convenience functions

--- file.py
+++ file.py
@@ -14,17 +14,26 @@ def pickle_cache(cache_filename, compare_filename_time=None, overwrite=False):
                 needs_redo |= os.path.getmtime(cache_filename) < os.path.getmtime(compare_filename_time)
             if not exists or needs_redo:
                 result = func(*args, **kwargs)
-                pathname = os.path.split(cache_filename)[0]
-                if not os.path.exists(pathname):
-                    os.makedirs(pathname)
-                with open(cache_filename, 'wb') as f:
-                    pickle.dump(result, f, protocol=2)
+                pkl_save(result, cache_filename)
             else:
-                with open(cache_filename, 'rb') as f:
-                    result = pickle.load(f)
+                result = pkl_load(cache_filename)
             return result
         return wrapper
     return decorator
+
+
+def pkl_save(obj, filename):
+    pathname = os.path.split(filename)[0]
+    if not os.path.exists(pathname):
+        os.makedirs(pathname)
+    with open(filename, 'wb') as f:
+        pickle.dump(obj, f, protocol=2)
+
+
+def pkl_load(filename):
+    with open(filename, 'rb') as f:
+        result = pickle.load(f)
+    return result
 
 
 def isnewer(src, dst):
