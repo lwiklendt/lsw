00c46c7fb20905414db1524a91e7067f56c58659
Lukasz Wiklendt <lwiklendt@gmail.com>
2021-03-25 12:13:26 +1030

    add options_for_show, and add delim to flatten_column_indexes

--- pandas.py
+++ pandas.py
@@ -10,8 +10,17 @@ def split(df, column):
         yield value, df_sub.drop(columns=[column])
 
 
-def flatten_column_indexes(df):
+def flatten_column_indexes(df, delim='_'):
     """
     https://stackoverflow.com/a/45214611/142712
     """
-    df.columns = ['_'.join(tuple(map(str, t))).rstrip('_') for t in df.columns.values]
+    df.columns = [delim.join(tuple(map(str, t))).rstrip(delim) for t in df.columns.values]
+
+
+def options_for_show(max_columns=None, expand_frame_repr=False, max_rows=60):
+    """
+    Every time I use pandas I have to look this up, so created a convencience function here.
+    """
+    pd.options.display.max_columns = max_columns
+    pd.options.display.expand_frame_repr = expand_frame_repr
+    pd.options.display.max_rows = max_rows
